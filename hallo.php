0c3732abf7db103d7263808fa9fa3a977c84f73a
esdotkom <esdotkom@gmail.com>
2026-02-10 10:52:41 +0700

    Add files via upload

---
+++ hallo.php
@@ -0,0 +1,94 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Title goes here</title>
+  <meta name="description" content="Description of your site goes here">
+  <meta name="keywords" content="keyword1, keyword2, keyword3">
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="main">
+<div class="page">
+<div class="header">
+<!-- <div class="banner">-->
+<h1>Your Company</h1>
+</div>
+<div class="topmenu">
+<ul>
+  <li style="border-left: medium none;"><a href="index.html">Home</a></li>
+  <li><a href="#">About Us</a></li>
+  <li><a href="#">Recent articles</a></li>
+  <li><a href="#">Email</a></li>
+  <li><a href="#">Resources</a></li>
+  <li><a href="#">Links</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+</div>
+</div>
+<div class="content">
+<!--Designed by--><a target="_blank" href="http://www.htmltemplates.net">
+<img src="images/copyright.gif" class="copyright" alt="html templates"></a>
+<div class="content-in">
+<div class="left-panel">
+<h2>Categories</h2>
+<div class="left-content">
+<ul>
+  <li><a href="#">List item (1)</a></li>
+  <li><a href="#">List item (2)</a></li>
+  <li><a href="#">List item (3)</a></li>
+  <li><a href="#">List item (4)</a></li>
+  <li><a href="#">List item (5)</a></li>
+  <li><a href="#">List item (6)</a></li>
+  <li><a href="#">List item (7)</a></li>
+  <li><a href="#">List item (8)</a></li>
+  <li><a href="#">List item (9)</a></li>
+</ul>
+</div>
+<div class="gap"></div>
+<h2>Recent Updates</h2>
+<div class="left-content">
+<ol>
+  <li><span><strong>Lorem Ipsum is simply dummy text</strong> of the
+printing and typesetting industry. Lorem Ipsum has been the.</span> <a href="#" class="more">read more...</a></li>
+  <li><span><strong>Lorem Ipsum is simply</strong> dummy text of the
+printing and typesetting industry.</span> <a href="#" class="more">read
+more...</a></li>
+</ol>
+</div>
+</div>
+<div class="right-panel">
+<div class="right-panel-in">
+<div class="row">
+<h2 class="title"><span>Welcome to Our <span>Company</span></span></h2>
+<div class="row2">
+<p>Isi data di sini</p>
+<p>Hallo Semuanya</p>
+<form action="hasilkirim.php" method="post">
+  Nama<input type="text" name="nama"><br>
+  Alamat<input type="text" name="alamat"><br>
+  kirim<input type="submit" value="kirim">
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="footer">
+<p>&copy; Copyright 2014. Designed by <a target="_blank" href="http://www.htmltemplates.net">html templates</a></p>
+<ul>
+  <li style="border-left: medium none;"><a href="index.html">Home</a></li>
+  <li><a href="#">About Us</a></li>
+  <li><a href="#">Recent articles</a></li>
+  <li><a href="#">Email</a></li>
+  <li><a href="#">Resources</a></li>
+  <li><a href="#">Links</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+</div>
+</div>
+</div>
+
+</body></html>
